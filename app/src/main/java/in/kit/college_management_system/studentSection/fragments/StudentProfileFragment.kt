2818aa56543aa5f07cc2efb0ec44a989272d5447
joyjitbhandari <joyjitbhanari91210@gmail.com>
2022-06-22 14:12:46 +0530

    changes in student profile, in student section

--- app/src/main/java/in/kit/college_management_system/studentSection/fragments/StudentProfileFragment.kt
+++ app/src/main/java/in/kit/college_management_system/studentSection/fragments/StudentProfileFragment.kt
@@ -11,6 +11,7 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
